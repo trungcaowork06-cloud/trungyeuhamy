f058e9618314e6d151ea4bfa26b1934363972da7
trungcaowork06-cloud <trungcaowork06@gmail.com>
2025-09-18 01:06:00 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hà My ới!',
+    introDesc: `Đồ ngốc chẳng yêu anh`,
+    btnIntro: ':(',
+    title: 'Bé có thể đừng ghét anh nữa được không ọ huuhuhu. Anh yêu em nhiều lắmmm',
+    desc: 'Anh nhớ em ',
+    btnYes: 'Vâng ạ, anh yêu em',
+    btnNo: 'KHÔNG?',
+    question:'Anh biết em sẽ nói em yêu anh mà',
+    btnReply: 'Gửi cho anh',
+    reply: 'Em yêu anh lắm ớ chồng iu',
+    mess: 'Anh biết mà nha đầu ngok 😘😋',
+    messDesc: 'M-mai cho anh gặp em nhó',
+    btnAccept: 'Dạ',
+    messLink: 'https://sf-static.upanhlaylink.com/img/image_20250918d0d525c1edc333702045e3a001da5718.jpg' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
